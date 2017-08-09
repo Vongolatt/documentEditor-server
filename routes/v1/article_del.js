30a4c8c7904fd665fc73b9edf4b8f909cc7323ef
Vongola <1085280247@qq.com>
2017-08-09 14:43:14 +0800

    修改删除文章接口

--- routes/v1/article_del.js
+++ routes/v1/article_del.js
@@ -9,16 +9,15 @@ module.exports = function (router) {
   router.post('/v1/article/del', ({ body: { id } }, res) => {
     async.waterfall([
       cb => {
-        Article.findByIdAndUpdate(id, { deletedAt: new Date() }).exec( err => {
-            console.log(err)
-            if (err) return cb(5001)
+        if (!id) return cb(4003, '文章id不能为空')
+        Article.findByIdAndUpdate(id, { deletedAt: new Date() }).exec( (err, doc) => {
+            if (err) return cb(5001, '系统错误')
+            if (!doc) return cb(5002, '没有找到文章')
             cb(200)
         })
       }
-    ], (status) => {
-      if (status === 4003) return res.json({ status, message: '文章id不能为空' }) 
-      if (status === 5001) return res.json({ status, message: '系统错误' })       
-      if (status === 5002) return res.json({ status, message: '没有找到文章' })
+    ], (status, result) => {
+      if (status !== 200) return res.json({ status, message: result })
       res.json({
         status: status,
         message: '删除成功'
