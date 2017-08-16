3a12e7ca31c0e385db5e9e3ab9975c8f622603e0
Vongola <1085280247@qq.com>
2017-08-16 16:30:12 +0800

    修改删除文档接口，直接删除目录

--- routes/v1/pub_book_del.js
+++ routes/v1/pub_book_del.js
@@ -7,7 +7,7 @@ module.exports = router => {
     waterfall([
       cb => {
         if (!id) return cb(4003, '文章id不能为空')
-       Pub.findByIdAndUpdate(id, { deletedAt: new Date() }).exec( (err, doc) => {
+       Pub.findByIdAndRemove(id).exec( (err, doc) => {
             if (err) return cb(5001, '系统错误')
             if (!doc) return cb(5002, '没有找到文章')
             cb(200)
