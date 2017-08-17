f71ff7ffc2c58ab1bce44a9b58c764dcf49f57bf
Vongola <1085280247@qq.com>
2017-08-17 16:25:50 +0800

    允许文章title重复

--- models/article.js
+++ models/article.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const articleSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'user' },
-  title: { type: String, required: true, unique: true },
+  title: { type: String, required: true },
   content: { type: String, default: '' },
   label: { type: String, default: 'API', enum: ['API', 'List'] },
   tag: [String],
-  sort: { type: Schema.Types.ObjectId, ref: 'sort' },
+  sort: { type: Schema.Types.ObjectId, ref: 'sort'},
   amend_times: { type: Number },
   deletedAt: { type: Date, expires: 10 }
 }, { timestamps: { createdAt: 'create_time', updatedAt: 'update_time' } })
