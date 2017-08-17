3f1add396a489919524157fd9919a68fb3f33b7f
Vongola <1085280247@qq.com>
2017-08-17 15:27:30 +0800

    删除分类时，同时删除分类中的索引

--- routes/v1/article_del.js
+++ routes/v1/article_del.js
@@ -2,25 +2,30 @@
  * 文章
  */
 
-const { Article, Recycle } = require('../../models')
+const { Article, Recycle, Sort } = require('../../models')
 const async = require('async')
 
 module.exports = function (router) {
-  router.post('/v1/article/del', ({ body: { id, sort } }, res) => {
+  router.post('/v1/article/del', ({ body: { id } }, res) => {
     async.waterfall([
       cb => {
-        if (sort) return cb(null)        
         if (!id) return cb(4003, '文章id不能为空')
-        Article.findByIdAndRemove(id).exec( (err, doc) => {
+        Article.findByIdAndRemove(id).exec( async (err, doc) => {
           if (err) return cb(5001, '系统错误')
           if (!doc) return cb(5002, '没有找到文章')
           const delArticle = doc.toObject()
-          Recycle.create(delArticle, (err, doc) => {
-            console.log(err)          
-            if (err) return cb(5001, '系统错误')
-            if (!doc) return cb(5002, '删除失败')
-            cb(200)            
-          })
+          // 从分类列表删除该文章id
+          const sort = await Sort.findByIdAndUpdate(doc.sort, { $pull: { articles: doc._id } } )
+          delArticle.sort = sort.name
+          cb(null, delArticle)
+        })
+      },
+      (delArticle, cb) => {
+        Recycle.create(delArticle, (err, doc) => {
+          console.log(err)          
+          if (err) return cb(5001, '系统错误')
+          if (!doc) return cb(5002, '删除失败')
+          cb(200)            
         })
       }
     ], (status, result) => {
